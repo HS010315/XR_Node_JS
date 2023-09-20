56aec12eca5da0ffc0f142131aa066fb00af12d9
HS010315 <hspower01@gmail.com>
2023-09-20 11:00:36 +0900

    Feat: "건설 API추가"
    건설 AIP 관련 내용 추가

--- Node/Unity_Express.js
+++ Node/Unity_Express.js
@@ -5,6 +5,69 @@ let users = [
     { id: 0, data: "UserData"}              //배열안에 오브젝트
 ];
 
+let buildingUnderConstruction = null;       //시간 데이터를 저장한다
+//let buildingUnderConstruction[10][10] + 오브젝트 형태가 들어가게
+
+//================== Game Logic =====================
+app.post('/startConstruction' , (req, res)=> {
+    const currentTime = new Date();         //스타트시 Const로 시간을 고정해 준다. DB -> 로 저장할 때는 형태 결정해야함
+    const constructionTime = new Date(currentTime.getTime() + 10000);   //현재 시간으로부터 10초 뒤 저장(건물 완성 10초)
+    buildingUnderConstruction = constructionTime;                       //완성 시간을 전역으로
+
+    let result = {                              //구조를 나중에 자세하게 설정
+        message : buildingUnderConstruction
+    };
+
+    console.log("SYSTEM : 건설 시작");
+
+    res.send({                                  //기본 커맨드로 전송
+        cmd : 1101,
+        nessage : "SYSTEM : 건설 시작 ",
+        result
+    });
+});
+app.get('/checkConstruction' , (req, res) => {
+
+    if(buildingUnderConstruction && new Date() >= buildingUnderConstruction)            // 시간이 지났으면
+    {
+        buildingUnderConstruction = null;
+
+        let result = {
+            message : buildingUnderConstruction
+        }
+
+        console.log("SYSTEM : 건설 완료");
+
+        res.send({                                  //기본 커맨드로 전송
+            cmd : 1101,
+            nessage : "SYSTEM : 건설 완료 ",
+            result
+        });
+        
+    }
+    else                    //설정한 시간 이전
+    {
+        let remainingTime = buildingUnderConstruction ? buildingUnderConstruction - new Date() : 0;
+        remainingTime = Math.max(0 , remainingTime);        //음수 시간을 0으로 설정
+
+        console.log("SYSTEM : 건설 중입니다. 남은 시간은 " + remainingTime + "ms");
+
+        let result = {
+            message : remainingTime
+        }
+        
+        res.send({                                  //기본 커맨드로 전송
+            cmd : 1101,
+            nessage : "SYSTEM : 건설 중입니다. ",
+            result
+        });
+    }
+
+});
+
+
+//=================== Game API ======================
+
 app.use(express.json());                   //json을 사용하겠다.
 
 app.get('/',(req ,res)=>{
